d009dacbb51c752905f6c77d36effcb4a6083152
svatal <swata@centrum.cz>
2024-12-10 18:10:57 +0100

    10 cleanup

--- src/10.ts
+++ src/10.ts
@@ -3,7 +3,6 @@ import { input } from "./10-input";
 import {
   IPosition,
   neighbors4,
-  posFromString,
   posToString,
   valueInMap,
 } from "./utils/position2D";
@@ -13,26 +12,6 @@ export function doIt(progress: (...params: any[]) => void) {
     .split(`\n`)
     .map((line) => line.split("").map((cell) => +cell));
   let first = 0;
-  parsed.forEach((line, y) =>
-    line.forEach((cell, x) => {
-      if (cell !== 0) return;
-      let poss = [{ x, y }];
-      let current = 0;
-      while (poss.length > 0 && current < 9) {
-        current++;
-        poss = [
-          ...new Set(
-            poss.flatMap((pos) =>
-              neighbors4(pos)
-                .filter((pos) => valueInMap(parsed)(pos) === current)
-                .map(posToString)
-            )
-          ),
-        ].map(posFromString);
-      }
-      first += poss.length;
-    })
-  );
   let second = 0;
   parsed.forEach((line, y) =>
     line.forEach((cell, x) => {
@@ -50,16 +29,15 @@ export function doIt(progress: (...params: any[]) => void) {
             )
             .reduce((acc, path) => {
               const key = posToString(path.pos);
-              if (acc.has(key)) {
-                acc.get(key)!.count += path.count;
-              } else {
-                acc.set(key, path);
-              }
-              return acc;
+              return acc.set(key, {
+                pos: path.pos,
+                count: path.count + (acc.get(key)?.count ?? 0),
+              });
             }, new Map<string, { pos: IPosition; count: number }>())
             .values(),
         ];
       }
+      first += poss.length;
       second += poss.reduce((acc, path) => acc + path.count, 0);
     })
   );
